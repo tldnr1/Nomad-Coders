7d2914d97d80b850b2c800a589823e7200b2157d
RnL <63404135+tldnr1@users.noreply.github.com>
2022-09-04 12:29:58 +0900

    weather-app
    이전에 있던 repo 정리

---
+++ weather-app/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Alert } from 'react-native';
+import Loading from "./Loading";
+import Weather from "./Weather";
+import * as Location from "expo-location";
+import axios from "axios";
+
+const API_KEY = "8cd43f82de279317ed492baef61791c9";
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  }
+  getWeather = async(latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather
+      }
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    
+    this.setState({
+      isLoading:false,
+      condition: weather[0].main,
+      temp
+    });
+  }
+  getLocation = async() => {
+    try {
+      await Location.requestForegroundPermissionsAsync();
+      const { 
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync({ accuracy:Location.Accuracy.High });
+      this.getWeather(latitude, longitude);
+    } catch (error) {
+      Alert.alert("Can't find you", "So sad");
+    }
+  }
+  componentDidMount() {
+    this.getLocation();
+  }
+  
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} />;
+  }
+}
